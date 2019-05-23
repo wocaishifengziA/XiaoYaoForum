8b5cc42ef611c035979eeff0af1cf40d679ddac1
wocaishifengziA <531629579@qq.com>
2019-05-24 01:09:08 +0800

    longin ok

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -3,8 +3,6 @@ import json
 import requests
 
 web_url = "http://127.0.0.1:8888"
-
-
 def test_sms():
     url = "{}/code/".format(web_url)
     data = {
@@ -12,4 +10,32 @@ def test_sms():
     }
     res = requests.post(url, json=data)
 
+    print(res.text)
+
+def test_register():
+    url = "{}/register/".format(web_url)
+    data = {
+        "mobile": "18782651256",
+        "code": "1234",
+        "password": "admin123"
+    }
+    res = requests.post(url, json=data)
+
     print(json.loads(res.text))
+
+def test_login():
+    # url = "{}/login/".format(web_url)
+    url = "http://127.0.0.1:8888/login/"
+    data = {
+        "mobile": "18782651256",
+        "password": "admin123"
+    }
+    res = requests.post(url, json=data)
+
+    print(json.loads(res.text))
+
+
+if __name__ == "__main__":
+    # test_sms()
+    # test_register()
+    test_login()
